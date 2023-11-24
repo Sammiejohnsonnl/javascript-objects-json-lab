5e9ee9a08a29adc708a63c33bca6117b2baeba31
Okunowo <c3055721@hallam.shu.ac.uk>
2023-11-24 10:24:27 +0000

    update time

--- js/clock.js
+++ js/clock.js
@@ -6,12 +6,20 @@ const ddigitalClock = document.getElementById("digitalClock")
 digitalClock.appendChild(myNode);
 myNode.innerHTML = "Welcome";
 // get time
+const updateTime = function() {
+    const updateTime = () => {
+    console.info("tick tock");
+  };
+setInterval(updateTime, 1000);
+
 const myDate = new Date();
 console.dir(myDate);
 myNode.innerHTML = myDate.toDateString();
-
+}
 // show only hours, minutes and seconds
 myNode.innerHTML = myDate.toLocaleTimeString([], 
 { hour: "2-digit", minute: "2-digit", second: "2-digit" });
+
+
 // end code
 })();
