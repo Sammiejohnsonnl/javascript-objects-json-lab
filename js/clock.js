ce34af73d0e45865e06b8a51bbfcb71c7875b44c
Okunowo <c3055721@hallam.shu.ac.uk>
2023-11-24 09:44:23 +0000

    Node

--- js/clock.js
+++ js/clock.js
@@ -2,7 +2,8 @@
 // start code
 console.info("Hello JS"); 
 const myNode = document.createElement("div");
-
+const ddigitalClock = document.getElementById("digitalClock")
+digitalClock.appendChild(myNode);
 
 // end code
 })();
